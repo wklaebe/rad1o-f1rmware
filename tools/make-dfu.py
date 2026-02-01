7e2fff0f79b112738ed14d1a2288f65dbfd3d118
Wilfried Klaebe <734953+wklaebe@users.noreply.github.com>
2026-02-01 23:18:11 +0100

    port tools/make-dfu.py to python3

--- tools/make-dfu.py
+++ tools/make-dfu.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # vim: set ts=4 sw=4 tw=0 et pm=:
 import struct
 import sys
@@ -42,27 +42,27 @@ else:
 	out = open(remainder[1],"wb")
 
 # ref. NXP UM10503 Table 24 (Boot image header description)
-header = ""
-header += struct.pack ('<B',int("11"+"011010",2)) # AES enc not active + No hash active
-header += struct.pack ('<B',int("11"+"111111",2)) # RESERVED + AES_CONTROL
+header = bytearray()
+header += struct.pack('<B',int("11"+"011010",2)) # AES enc not active + No hash active
+header += struct.pack('<B',int("11"+"111111",2)) # RESERVED + AES_CONTROL
 size=os.path.getsize(in_file)
-size=(size+511)/512 # 512 byte blocks, rounded up
+size=int((size+511)/512) # 512 byte blocks, rounded up
 header += struct.pack('<H',size)                  # (badly named) HASH_SIZE
 header += struct.pack('8B',*[0xff] *8)            # HASH_VALUE (unused)
 header += struct.pack('4B',*[0xff] *4)            # RESERVED
 
 out.write( header )
 
-infile=open(in_file,"rb").read()
+infile = open(in_file,"rb").read()
 out.write( infile )
 
-suffix= ""
-suffix+= struct.pack('<H', did)  # bcdDevice
-suffix+= struct.pack('<H', pid)  # idProduct
-suffix+= struct.pack('<H', vid)  # idVendor
-suffix+= struct.pack('<H', spec) # bcdDFU
-suffix+= b'DFU'[::-1]            # (reverse DFU)
-suffix+= struct.pack('<B', 16)   # suffix length
+suffix = bytearray()
+suffix += struct.pack('<H', did)  # bcdDevice
+suffix += struct.pack('<H', pid)  # idProduct
+suffix += struct.pack('<H', vid)  # idVendor
+suffix += struct.pack('<H', spec) # bcdDFU
+suffix += b'DFU'[::-1]            # (reverse DFU)
+suffix += struct.pack('<B', 16)   # suffix length
 
 out.write( suffix )
 
